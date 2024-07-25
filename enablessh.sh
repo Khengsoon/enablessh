5b3e48604c9cc43d595f47dbd9d27232d1de7430
Khengsoon <Laukhengsoon@gmail.com>
2024-07-25 14:18:12 +0800

    Create enablessh.sh

---
+++ enablessh.sh
@@ -0,0 +1,3 @@
+sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' -e 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+systemctl restart sshd
